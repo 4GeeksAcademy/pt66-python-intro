1f6a803fd9e5dddd8ee78375fec457a335f8032c
Shane B <dotfortun@users.noreply.github.com>
2024-06-17 17:59:14 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+hello = """Hello world.
+This is a dev container for a seminar hosted by 4Geeks Academy,
+where we'll be building a simple API using Flask.
+"""
+
+if __name__ == "__main__":
+    print(hello)
